0b959fecc4d8eb3930d45cb0f5de07e6bf8ad79f
godsbane <godsbane@gmail.com>
2022-07-19 23:05:35 -0400

    use local variables in setsettings. some shellcheck

--- functions/setSetting.sh
+++ functions/setSetting.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 setSetting () {
-	var=$1
-	new_val=$2	
-	file="$HOME/emudeck/settings.sh"
-	settingExists=$(grep -rnw $file -e $var)
+	local var=$1
+	local new_val=$2	
+	local file="$HOME/emudeck/settings.sh"
+	local settingExists=$(grep -rnw "$file" -e "$var")
 	if [[ $settingExists == '' ]]; then
 		#insert setting to end
-		sed -i -e '$a\'"$var=$new_val" $FILE
+		sed -i -e '$a\'"$var=$new_val" "$FILE"
 	elif [[ ! $settingExists == '' ]]; then
 		echo "Old value $settingExists"
 		#update setting
